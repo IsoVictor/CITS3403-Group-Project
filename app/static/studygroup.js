42606eaabe09df035bbc5505dc4461b48bcce9eb
IsoVictor <iso.victor08@gmail.com>
2024-05-11 20:09:06 +0800

    ability to show error when no study group code

--- app/static/studygroup.js
+++ app/static/studygroup.js
@@ -1,13 +1,19 @@
 function filterGroups() {
+    warning.style.display = 'none';
     var input = document.getElementById('unit_code').value.toUpperCase();
     var groups = document.getElementsByClassName('group-item');
+    var found = false;
 
     for (var i = 0; i < groups.length; i++) {
         var unitCode = groups[i].getAttribute('data-unit-code').toUpperCase();
         if (unitCode.indexOf(input) > -1) {
             groups[i].style.display = '';
+            found = true;
         } else {
             groups[i].style.display = 'none';
         }
+    }
+    if (!found) {
+        warning.style.display = 'block';
     }
 }
