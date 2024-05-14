af56852c813cc12213c8047c0aacdd8b4a53fb94
Rohnan Klifunis <sallyklif@Sallys-Air.gateway>
2024-05-14 15:04:17 +0800

    altered innit to initialise login manager

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user
+from app.config import Config
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///study_app.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-login = LoginManager(flaskApp)
-login.login_view = 'login'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
