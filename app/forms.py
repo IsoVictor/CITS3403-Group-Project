0b6f3fc0edca8a5c34e54c9eb12bb135601e2628
Rohnan Klifunis <sallyklif@Sallys-Air.gateway>
2024-05-14 13:41:16 +0800

    added correct import to forms

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,18 @@
-from wtfroms import PasswordField, StringField, SubmitField, SelectField
+from flask import render_template, flash, redirect, url_for, request
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired])
-    password = PasswordField("Password", validators=[DataRequired])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
-    
+
 
 class SignupForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired])
-    studentnumber = StringField("Student Number", validators=[DataRequired])
-    username = StringField("Username", validators=[DataRequired])
-    password = PasswordField("Password", validators=[DataRequired])
-    confirmpassword = PasswordField("Confirm Password", validators=[DataRequired])
-    submit = SumbitField("Create Account")
+    email = StringField("Email", validators=[DataRequired()])
+    studentnumber = StringField("Student Number", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirmpassword = PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Create Account")
