3a288f94ea350f38f016ace5846d12510acc470a
Rohnan Klifunis <sallyklif@Sallys-Air.gateway>
2024-05-14 13:39:26 +0800

    added login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, login_user
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///study_app.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(flaskApp)
+login.login_view = 'login'
 
 app.debug = True
 
