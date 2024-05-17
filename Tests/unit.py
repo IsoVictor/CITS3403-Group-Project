e6e4f06eab4203ac974f2444908aa0826940aef3
Rohnan Klifunis <sallyklif@Sallys-Air.gateway>
2024-05-17 21:06:43 +0800

    added folder for tests and file unit test

---
+++ Tests/unit.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from app import create_app, db
+from app.config import TestConfig
+from app.controllers import GroupCreationError, create_group
+
+class BasicUnitTests(TestCase):
+
+    def setUp(self):
+        testApp = create_app(TestConfig)
+        self.app_context = testApp.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    
