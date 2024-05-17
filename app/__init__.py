07ef5703a717eba5a52ad5bac81ac1818eec2d91
Rohnan Klifunis <sallyklif@Sallys-Air.gateway>
2024-05-17 20:43:10 +0800

    changed innit to suit blueprint tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from flask import Flask
-from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user
 from app.config import Config
 
+db = SQLAlchemy()
+login = LoginManager()
+login.login_view = 'login'
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+def create_app(config): 
+    app = Flask(__name__)
+    app.config.from_object(config)
+    from app.blueprints import main
+    app.register_blueprint(main)
+    db.init_app(Fapp)
+    login.init_app(app)
+
+    return app
 
 
-app.debug = True
+
 
 from app import routes, models
