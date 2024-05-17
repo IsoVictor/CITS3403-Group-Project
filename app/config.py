c583f184a63ede066a40b0d9121e88f687a9afbc
Rohnan Klifunis <sallyklif@Sallys-Air.gateway>
2024-05-17 20:52:05 +0800

    altered config to suit blueprint and tests

--- app/config.py
+++ app/config.py
@@ -6,3 +6,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
