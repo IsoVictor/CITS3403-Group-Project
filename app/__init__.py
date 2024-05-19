5281867980e449d59e95082f697d9fcdeaf55e9f
ann250902 <150408221+ann250902@users.noreply.github.com>
2024-05-19 08:10:44 +0800

    feat: Implement image upload function and update user profile info

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user
 from app.config import Config
-
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +14,8 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 app.debug = True
 
 from app import routes, models
